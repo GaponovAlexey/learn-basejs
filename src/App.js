c97909f50051b7ad345210685a805daf9df9999c
Kekelberi <alexei.gaponov@gmail.com>
2022-03-28 22:25:08 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 function App() {
   return (
-    <div >
-      start
+    <div>
+      <div className='os-phrases'>
+        <h2>Sometimes it's better</h2>
+        <h2>to hide</h2>
+        <h2>in order to</h2>
+        <h2>survive evil</h2>
+        <h2>Thanatos</h2>
+        <h2>This fall</h2>
+        <h2>Prepare</h2>
+        <h2>Refresh to replay</h2>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
